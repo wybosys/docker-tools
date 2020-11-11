a731808efabec8431af237144283651a110dd53b
wybosys <wybosys@gmail.com>
2020-11-11 18:46:01 +0800

    prj

--- delete-images.py
+++ delete-images.py
@@ -4,10 +4,11 @@ import os
 import docker
 import sys
 
+docker.cmd('container prune -f')
 all = docker.cmd('image ls')
 
 for each in all:
-    data = each.split(' ')
+    data = list(filter(lambda e: e != '', each.split(' ')))
     name = data[0]
     id = data[2]
     print('删除' + name)
